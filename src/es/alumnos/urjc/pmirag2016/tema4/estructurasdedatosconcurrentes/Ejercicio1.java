69d80eecc9e09e236c5abd0af07f1afd3c9aa2de
RamsayBoy <38114695+RamsayBoy@users.noreply.github.com>
2020-06-08 13:26:08 +0200

    Add ejercicio EDs concurrentes

---
+++ src/es/alumnos/urjc/pmirag2016/tema4/estructurasdedatosconcurrentes/Ejercicio1.java
@@ -0,0 +1,46 @@
+package es.alumnos.urjc.pmirag2016.tema4.estructurasdedatosconcurrentes;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+ * Se desea implementar de forma concurrente un programa que busca ficheros con el mismo
+ * nombre dentro de una carpeta.
+ *
+ * La búsqueda se realiza recursivamente en unas carpetas dentro de otras.
+ *
+ * Se proporciona la versión secuencial del programa.
+ *
+ * Por simplicidad, en la carpeta raíz no hay ficheros y se crearán tantos hilos
+ * como carpetas.
+ */
+public class Ejercicio1 {
+
+    private static final String PATHNAME = "C:\\Dir";
+    
+    private static Map<String, String> duplicates = new HashMap<>();
+
+    public static void findDuplicates(File root) {
+        if (root.isDirectory()) {
+            for (File file : root.listFiles()) {
+                if (file.isDirectory()) {
+                    findDuplicates(file);
+                } else {
+                    String path = duplicates.get(file.getName());
+                    if (path == null) {
+                        duplicates.put(file.getName(), file.getAbsolutePath());
+                    } else {
+                        System.out.println("Found duplicate file: " + file.getName());
+                        System.out.println("    " + path);
+                        System.out.println("    " + file.getAbsolutePath());
+                    }
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        findDuplicates(new File(PATHNAME));
+    }
+}
